22d45f96412fb2e901f645814e643301800bc026
Foyaz-Ahmed <ahmedfoyaz0000@gmail.com>
2022-02-26 21:55:43 +0600

    props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Hello from './mycomponents/Hello';
 import Welcome from './mycomponents/Welcome';
+import PropsFunction from './mycomponents/Props';
+
 function App() {
   return (
     <div className="App">
-
       <Hello></Hello>
       <Welcome></Welcome>
+
+      {/* Props Function */}
+      <PropsFunction name="Foyaz Ahmed" age="21"></PropsFunction>
+      <PropsFunction name="Bulbul Ahmed" age="22"></PropsFunction>
+      <PropsFunction name="Mahin Ahmed" age="23"></PropsFunction>
+      <PropsFunction name="Nazim Ahmed" age="24"></PropsFunction>
     </div>
   );
 }
---
+++ my-app/src/mycomponents/Props.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PropsFunction(props){
+    return(
+        <h1>Name: {props.name} and age: {props.age}</h1>
+    );
+}
+
+export default PropsFunction;
