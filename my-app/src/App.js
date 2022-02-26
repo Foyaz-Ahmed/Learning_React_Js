f4e4dbac7fc4fd40ab15192f3cf4fc5020a48448
Foyaz-Ahmed <ahmedfoyaz0000@gmail.com>
2022-02-26 21:36:28 +0600

    Functional Component & Class Component

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './mycomponents/Hello';
+import Welcome from './mycomponents/Welcome';
+function App() {
+  return (
+    <div className="App">
+
+      <Hello></Hello>
+      <Welcome></Welcome>
+    </div>
+  );
+}
+// something 
+export default App;
---
+++ my-app/src/mycomponents/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello()
+    {
+        
+        return(
+            <h1>I am from Functional Component</h1>
+        );
+    }
+
+export default Hello;
